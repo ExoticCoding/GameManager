c04105394d1a3eb40f888e698796b67673b15513
ExoticCoding <coreyshupe@oak-hill.net>
2016-04-24 22:17:19 -0400

    Let GFX deal with the rendering and added the "escape" option to show

--- GameManagerGit/src/games/pong/GFX.java
+++ GameManagerGit/src/games/pong/GFX.java
@@ -1,5 +1,7 @@
 package games.pong;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import javax.swing.JPanel;
@@ -15,6 +17,67 @@ public class GFX extends JPanel{
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		pong.render((Graphics2D) g);
+		Graphics2D g2 = (Graphics2D) g;
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, pong.width, pong.height);
+		if(pong.ingame){
+			drawInGame(g2);
+		}else{
+			drawPreGame(g2);
+		}
+		pong.drawBorder(g2, pong.width, pong.height);
 	}
+	
+	private final void drawPreGame(Graphics2D g){
+		g.setFont(new Font("Verdana", Font.BOLD, 50));
+		g.setColor(pong.menuOption == 1 ? Color.CYAN : Color.BLUE);
+		g.drawString("-> Play game", 20, 50);
+		g.setColor(pong.menuOption == 2 ? Color.CYAN : Color.BLUE);
+		g.drawString("-> Exit pong", 20, 150);
+		g.setColor(pong.menuOption == 3 ? Color.CYAN : Color.BLUE);
+		g.drawString("-> Edit settings", 20, 250);
+		g.setColor(Color.GRAY);
+		g.setFont(new Font("Verdana", Font.ITALIC, 20));
+		for(int i = 0; i < pong.actions.size(); i++) g.drawString(pong.actions.get(i), 393, (i * 40) + 450);
+		if(pong.winner != null){
+			String[] strings = {
+					"You have beaten the bot!", "You have been beaten by the bot...", "Congratulations!", "Better luck next time"
+			};
+			g.setColor(Color.MAGENTA);
+			g.setFont(new Font("Times New Roman", Font.ITALIC, 40));
+			g.drawString(strings[pong.winner.player ? 0:1], 20, 340);
+			g.drawString(strings[pong.winner.player ? 2:3], 20, 380);
+		}
+	}
+	
+	private final void drawInGame(Graphics2D g){
+		g.setColor(Color.RED);
+		String string = "Rally: " + pong.rally;
+		g.setFont(new Font("TimesRoman", Font.PLAIN, 35)); 
+		if(pong.rally >= 10) g.drawString(string, 155 - (10f * string.length()), 40);
+		g.setFont(new Font("TimesRoman", Font.PLAIN, 100)); 
+		g.drawString(String.valueOf(pong.player.score), 225, 75);
+		g.drawString(String.valueOf(pong.bot.score), 745, 75);
+		g.setFont(new Font("TimesRoman", Font.HANGING_BASELINE, 60)); 
+		pong.ball.render(g);
+		pong.player.render(g);
+		pong.bot.render(g);
+		g.setColor(Color.WHITE);
+		if(!pong.paused && pong.countdown < 0) g.drawLine(pong.width / 2 - 15, 0, pong.width / 2 - 15, pong.height);
+		if(pong.countdown >= 0){
+			g.setColor(Color.RED);
+			g.setFont(new Font("Times New Roman", Font.ITALIC, 80));
+			if(pong.countdown > 80) g.drawString("3...", pong.width / 2-75, pong.height / 2 - 75);
+			else if(pong.countdown > 40) g.drawString("2...", pong.width / 2-75, pong.height / 2 - 75);
+			else g.drawString("1...", pong.width / 2-75, pong.height / 2 - 75);
+			pong.countdown--;
+		}
+		if(pong.paused){
+			g.setColor(Color.RED);
+			g.setFont(new Font("Times New Roman", Font.ITALIC, 30));
+			g.drawString("Game Paused", 400, 175);
+			g.drawString("Press space to continue.", 340, 225);
+		}
+	}
+	
 }
--- GameManagerGit/src/games/pong/Pong.java
+++ GameManagerGit/src/games/pong/Pong.java
@@ -1,8 +1,5 @@
 package games.pong;
 
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
@@ -27,19 +24,20 @@ public class Pong extends GameBase{
 	protected int maxScore = 1, menuOption = 1, gameSpeed = 1, botCooldown, countdown, rally;
 	protected final int width = 1000, height = 600;
 	
-	private final Timer timer;
-	private final GFX gfx;
-	private final SettingsFrame settings; 
-	private final JFrame frame;
-	private final Ball ball;
-	private final Paddle player, bot;
-	private final List<String> actions = Arrays.asList(
+	protected final Timer timer;
+	protected final GFX gfx;
+	protected final SettingsFrame settings; 
+	protected final JFrame frame;
+	protected final Ball ball;
+	protected final Paddle player, bot;
+	protected final List<String> actions = Arrays.asList(
 					"UP / W -> Moves paddle up (or previous option on menu)", 
 					"DOWN / S -> Moves paddle down (or next option on menu)", 
-					"Space -> Pauses game until pressed again.");
+					"Space -> Pauses game until pressed again.", 
+					"Escape -> Exits game (while ingame)");
 	
-	private boolean ingame = false, paused = false, up = false, down = false;
-	private Paddle winner;
+	protected boolean ingame = false, paused = false, up = false, down = false;
+	protected Paddle winner;
 	
 	public Pong() {
 		frame = createFrame(width, height);
@@ -85,60 +83,6 @@ public class Pong extends GameBase{
 		}
 	}
 	
-	public void render(Graphics2D g){
-		g.setColor(Color.BLACK);
-		g.fillRect(0, 0, width, height);
-		if(ingame){
-			g.setColor(Color.RED);
-			String string = "Rally: " + rally;
-			g.setFont(new Font("TimesRoman", Font.PLAIN, 35)); 
-			if(rally >= 10) g.drawString(string, 155 - (10f * string.length()), 40);
-			g.setFont(new Font("TimesRoman", Font.PLAIN, 100)); 
-			g.drawString(String.valueOf(player.score), 225, 75);
-			g.drawString(String.valueOf(bot.score), 745, 75);
-			g.setFont(new Font("TimesRoman", Font.HANGING_BASELINE, 60)); 
-			ball.render(g);
-			player.render(g);
-			bot.render(g);
-			g.setColor(Color.WHITE);
-			if(!paused && countdown < 0) g.drawLine(width / 2 - 15, 0, width / 2 - 15, height);
-			if(countdown >= 0){
-				g.setColor(Color.RED);
-				g.setFont(new Font("Times New Roman", Font.ITALIC, 80));
-				if(countdown > 80) g.drawString("3...", width / 2-75, height / 2 - 75);
-				else if(countdown > 40) g.drawString("2...", width / 2-75, height / 2 - 75);
-				else g.drawString("1...", width / 2-75, height / 2 - 75);
-				countdown--;
-			}
-			if(paused){
-				g.setColor(Color.RED);
-				g.setFont(new Font("Times New Roman", Font.ITALIC, 30));
-				g.drawString("Game Paused", 400, 175);
-				g.drawString("Press space to continue.", 340, 225);
-			}
-		}else{
-			g.setFont(new Font("Verdana", Font.BOLD, 50));
-			g.setColor(menuOption == 1 ? Color.CYAN : Color.BLUE);
-			g.drawString("-> Play game", 20, 50);
-			g.setColor(menuOption == 2 ? Color.CYAN : Color.BLUE);
-			g.drawString("-> Exit pong", 20, 150);
-			g.setColor(menuOption == 3 ? Color.CYAN : Color.BLUE);
-			g.drawString("-> Edit settings", 20, 250);
-			g.setColor(Color.GRAY);
-			g.setFont(new Font("Verdana", Font.ITALIC, 20));
-			for(int i = 0; i < actions.size(); i++) g.drawString(actions.get(i), 393, (i * 40) + 500);
-			if(winner != null){
-				String[] strings = {
-						"You have beaten the bot!", "You have been beaten by the bot...", "Congratulations!", "Better luck next time"
-				};
-				g.setColor(Color.MAGENTA);
-				g.setFont(new Font("Times New Roman", Font.ITALIC, 40));
-				g.drawString(strings[winner.player ? 0:1], 20, 340);
-				g.drawString(strings[winner.player ? 2:3], 20, 380);
-			}
-		}
-		drawBorder(g, width, height);
-	}
 	public void endGame(Paddle winner){
 		ball.resetBall();
 		player.y = 225;
